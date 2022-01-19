8d4a66c4c9567ccda8d39b50fa617006dda83e1f
jackrobert0220 <jackrobert0220@gmail.com>
2022-01-18 22:30:27 -0800

    cleaned up comments routes

--- routes/comments.js
+++ routes/comments.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require("express").Router();
 const commentsCtrl = require('../controllers/comments');
 
 router.post('/movies/:id/reviews', commentsCtrl.create);
