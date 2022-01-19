a388528a2cf7ae61bf205600628ad7eecf2b376c
Nat <fthbby@gmail.com>
2022-01-18 22:31:31 -0800

    added controllers for comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const commentsCtrl = require('../controllers/comments');
+const commentsCtrl = require('../controllers');
 
-router.post('/movies/:id/reviews', commentsCtrl.create);
+router.post('/movies/:id/reviews', commentsCtrl.posts.create);
 
 
 
