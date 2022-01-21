77ad8fb65e018ce9db759fe638db5675f8d3d34f
Nat <fthbby@gmail.com>
2022-01-20 22:55:23 -0800

    adding styling to all the books views

--- routes/comments.js
+++ routes/comments.js
@@ -9,9 +9,9 @@ router.post('/:id/comments', commentsCtrl.comments.create);
 
 router.delete('/:id/comments', commentsCtrl.comments.destroy);
 
-router.post('/:id/comments', commentsCtrl.commentsBook.create);
+// router.post('/:id/comments', commentsCtrl.commentsBook.create);
 
-router.delete('/:id/comments', commentsCtrl.commentsBook.destroy);
+// router.delete('/:id/comments', commentsCtrl.commentsBook.destroy);
 
 
 module.exports = router;
