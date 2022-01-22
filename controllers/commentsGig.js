c0eaf8be3edff1f8e1edf0b524f043edbe77cd09
Nat <fthbby@gmail.com>
2022-01-22 12:05:10 -0800

    changed css for header and footer

--- controllers/commentsGig.js
+++ controllers/commentsGig.js
@@ -10,18 +10,54 @@ function create(req, res) {
     });
   }
 
-  const destroy = (req, res)=> {
-    db.Gig.findByIdAndDelete(req.params.id, (err, foundGig) => {
-        if (err) return res.send(err);
+//   const destroy = (req, res)=> {
+//     db.Gig.findByIdAndDelete(req.params.id, (err, foundGig) => {
+//         if (err) return res.send(err);
 
-        return res.redirect(`/gigs/${gigs._id}`)
-    });
-};
+//    return res.redirect(`/gigs/${gigs._id}`)
+//     });
+// };
+
+
+
+
+// const destroy = (req,res) => {
+//   db.Gig.findById(req.params.id, function(err, gigs){
+//     if (!err) {
+//       gigs.comments[0].remove();
+//       gigs.save(function(err){
+//         res.redirect(`/gigs/${gigs._id}`)
+//       })
+//     }
+//   })
+// }
+
+// const destroy = (req, res) => {
+//   db.Gig.findById(req.params.id, (err, gigs)=>{
+//     if (err) return res.send(err);
+//     gigs.comments.remove(req.params.id);
+//     gigs.save(function(err){
+//       res.redirect(`/gigs/${gigs._id}`)
+//     })
+//   })
+
+// }
+
+
+
+const destroy = (req, res) => {
+  db.Gig.findById(req.params.id, (err, gigs)=> {
+    if(err) return res.send(err);
+    gigs.comments.id(req.params.id).remove();
+    gigs.save(function(err){
+      res.redirect(`/gigs/${gigs._id}`)
+    })
+  })
+}
+
 
 
 module.exports = {
     create,
     destroy,
 }
-
-
